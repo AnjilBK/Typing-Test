b00ec1d0cf01a3a0c820ee73a89664d37da0792e
Maverick <074bct529.rabin@pcampus.edu.np>
2018-01-08 09:36:52 +0545

    Changes for linux

--- timer.c
+++ timer.c
@@ -1,20 +1,11 @@
-#include<stdio.h>
-#include<windows.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 int main()
 {
-  int hour, min, sec;
-  for(hour=0; hour<24; hour++){
-    for(min=0; min<59; min++){
-      for(sec=0; sec<59; sec++){
-        clrscr();
-        printf("\t\t%d\t\t: %d\t\t: %d\t\t",hour,min,sec);
-        printf("\n");
-
-        Sleep(1000);
-      }
-    }
-  }
+	printf("check\n");
+	sleep(3);
+	printf("Check");
 }
 
 
