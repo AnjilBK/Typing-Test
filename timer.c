751d7323516ce28f41327b807bed2cbbe294142f
Maverick <074bct529.rabin@pcampus.edu.np>
2018-01-10 22:41:16 +0545

    Made able to run in Linux

--- timer.c
+++ timer.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 int main()
 {
-	printf("check\n");
-	sleep(3);
-	printf("Check");
-}
-
-
-void clrscr()
-{
-    system("@cls||clear");
+  int hour, min, sec;
+  for(hour=0; hour<24; hour++){
+    for(min=0; min<59; min++){
+      for(sec=0; sec<59; sec++){
+		system("clear");
+        printf("\t\t%d\t\t: %d\t\t: %d\t\t",hour,min,sec);
+        printf("\n");
+        sleep(1);
+      }
+    }
+  }
 }
